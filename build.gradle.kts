19972f9529518a41c9944fc25d816722b3cbf237
Techcable <Techcable@techcable.net>
2023-01-19 23:55:04 -0700

    Set a Automatic-Module-Name for Java 9 compat
    Declare official compatibility with Java 7 (checked by compiler)
    
    The Automatic-Module-Name is pure metadata on Java 7/8, it is only
    interpreted specially on Java 9.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,3 +14,17 @@ dependencies {
     // Used for performance tests
     testImplementation("org.javolution:javolution:5.3.1")
 }
+
+tasks.compileJava {
+    // NOTE: This version is considered 'obselete'
+    //
+    // TODO: Upgrade to requiring Java 8 or higher
+    options.release.set(7)
+}
+
+tasks.jar {
+    manifest {
+        attributes("Automatic-Module-Name" to "org.ahmadsoft.ropes")
+    }
+}
+
